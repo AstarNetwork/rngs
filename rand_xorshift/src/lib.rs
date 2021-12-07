0d38600fc3bbf244bbe7606ce4ccb5ca05787028
NoCtrlZ <phantomofrotten@gmail.com>
2021-12-07 09:38:47 +0900

    add parity feature

--- rand_xorshift/src/lib.rs
+++ rand_xorshift/src/lib.rs
@@ -18,9 +18,11 @@
 #![no_std]
 
 use core::num::Wrapping as w;
-use core::fmt;
+use core::{fmt, mem};
 use rand_core::{RngCore, SeedableRng, Error, impls, le};
 #[cfg(feature="serde1")] use serde::{Serialize, Deserialize};
+use parity_scale_codec::{Decode, Encode, Input, Error as ParityError};
+use sp_std::vec::Vec;
 
 /// An Xorshift random number generator.
 ///
@@ -43,6 +45,50 @@ pub struct XorShiftRng {
     y: w<u32>,
     z: w<u32>,
     w: w<u32>,
+}
+
+impl Encode for XorShiftRng {
+    fn encode(&self) -> Vec<u8>  {
+        let mut r = [0u8; mem::size_of::<XorShiftRng>()];
+        r[0] = (self.x.0 % 2_i32.pow(8) as u32) as u8;
+        r[1] = (self.x.0 / 2_i32.pow(8) as u32) as u8;
+        r[2] = (self.x.0 / 2_i32.pow(16) as u32) as u8;
+        r[3] = (self.x.0 / 2_i32.pow(24) as u32) as u8;
+        r[4] = (self.y.0 % 2_i32.pow(8) as u32) as u8;
+        r[5] = (self.y.0 / 2_i32.pow(8) as u32) as u8;
+        r[6] = (self.y.0 / 2_i32.pow(16) as u32) as u8;
+        r[7] = (self.y.0 / 2_i32.pow(24) as u32) as u8;
+        r[8] = (self.z.0 % 2_i32.pow(8) as u32) as u8;
+        r[9] = (self.z.0 / 2_i32.pow(8) as u32) as u8;
+        r[10] = (self.z.0 / 2_i32.pow(16) as u32) as u8;
+        r[11] = (self.z.0 / 2_i32.pow(24) as u32) as u8;
+        r[12] = (self.w.0 % 2_i32.pow(8) as u32) as u8;
+        r[13] = (self.w.0 / 2_i32.pow(8) as u32) as u8;
+        r[14] = (self.w.0 / 2_i32.pow(16) as u32) as u8;
+        r[15] = (self.w.0 / 2_i32.pow(24) as u32) as u8;
+        r.to_vec()
+    }
+}
+
+impl Decode for XorShiftRng {
+    fn decode<I: Input>(input: &mut I) -> Result<Self, ParityError> {
+        let mut buf = [0u8; mem::size_of::<XorShiftRng>()];
+		input.read(&mut buf).unwrap();
+        let mut xyzw: [u32; 4] = [0; 4];
+        for i in 0..xyzw.len() {
+            let mut num = 0;
+            for k in 0..4 {
+                num += buf[(i * 4) + k] as u32 * 2_i32.pow(8 * k as u32) as u32;
+            }
+            xyzw[i] = num;
+        }
+        Ok(XorShiftRng {
+            x: w(xyzw[0]),
+            y: w(xyzw[1]),
+            z: w(xyzw[2]),
+            w: w(xyzw[3]),
+        })
+    }
 }
 
 // Custom Debug implementation that does not expose the internal state
